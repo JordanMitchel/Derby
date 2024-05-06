c59ef6ada91b87d095559301c5ed5f31947c997f
JordanMitchel <jmitchel24@gmail.com>
2024-05-06 07:02:49 +0100

    HangFireJob configured

---
+++ Derby.API/Config/HangfireJobConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Derby.API.Services;
+using Hangfire;
+
+namespace Derby.API.Config
+{
+	public static class HangfireJobConfiguration
+	{
+        public static void ConfigureJobs(this WebApplication app)
+        {
+            app.Services.CreateScope();
+
+            app.Services.GetService<ICronService>().FireOnceJob(null);
+            app.Services.GetService<ICronService>().RecurringDailyInstrumentJob();
+            //app.Services.GetService<ICronService>().FireOnceJob(null);
+            //app.Services.GetService<ICronService>().FireOnceJob(null);
+
+        }
+    }
+}
+
